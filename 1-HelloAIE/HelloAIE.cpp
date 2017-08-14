145062558830d1290e4289555abbbb6272d4c480
s179067 <s179067@SEA-8D0AB9>
2017-08-14 12:45:59 -0700

    Add project files.

---
+++ 1-HelloAIE/HelloAIE.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main()
+{
+	//hello
+	std::cout << "Hello AIE" << std::endl;
+
+	std::cout << std::endl;
+	// All my song choices that i love. Your opinion doesnt matter, its my mix
+	std::cout << "It must really suck to be four year strong right now - Four Year Strong" << std::endl;
+	std::cout << "One step closer - Linkin Park" << std::endl;
+	std::cout << "Paranoia - A Day To Remember" << std::endl;
+	std::cout << "Tonight We Feel Alive(On A Saturday) - Four Year Strong" << std::endl;
+	std::cout << "Bullfight - A Day To Remeber" << std::endl;
+	std::cout << "Naivety - A Day To Remeber" << std::endl;
+	std::cout << "Megalovania - NateWantsToBattle" << std::endl;
+	std::cout << "Let Me Try - NateWantsToBattle" << std::endl;
+	std::cout << "In The End - Linkin Park" << std::endl;
+	std::cout << "If It Means A Lot To You - A Day To Remember" << std::endl;
+	std::cout << "I'm Made Of Wax, Larry, What Are You Made Of - A Day To Remember" << std::endl;
+	std::cout << "Find My Way Back - Four Year Strong" << std::endl;
+	std::cout << "One Step At A Time - Four Year Strong" << std::endl;
+	std::cout << "Cure For The Itch - Linkin Park" << std::endl;
+	std::cout << "Bad Vibrations - A Day To Remember" << std::endl;
+	std::cout << "99 - NateWantsToBattle" << std::endl;
+	std::cout << "What The Hell Is A Gigawatt - Four Year Strong" << std::endl;
+	std::cout << "We Got This - A Day To Remember" << std::endl;
+	std::cout << "Wasting Time(Eternal Summer) - Four Year Strong" << std::endl;
+
+	std::cout << std::endl;
+	//counting up
+	for (int i = 0; i < 6; i++)
+	{
+		std::cout << i << std::endl;
+	}
+	
+	std::cout << std::endl;
+	//counting down
+	for (int i = 5; i >= 0; i--)
+	{
+		std::cout << i << std::endl;
+	}
+
+	std::cout << std::endl;
+	system("pause");
+
+}
