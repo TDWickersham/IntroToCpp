07d5bc3da679647eba9749ffe63a9253a936d523
Caleb Seay <s179067@students.aie.edu.eu>
2017-08-21 12:57:21 -0700

    commit

---
+++ Project2/Loops.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	/*int loop = -1;
+	while (loop < 9)
+	{
+		loop = loop++;
+		cout << loop << endl;
+	}
+	cout << endl;
+
+	int iterations = 10;
+	for (int i = 0; i < iterations; i++)
+	{
+		cout << i << endl;
+	}
+	cout << endl;
+
+	int j = 0;
+	do
+	{
+		cout << j << endl;
+		++j;
+	} while (j < 10);
+
+	cout << endl;
+
+	do 
+	{
+		int a;
+		cout << "Give me a number between 1 and 10: ";
+		cin >> a;
+		cout << endl;
+		if (a >= 1 && a <= 10) 
+		{
+			cout << "You listened!!" << endl;
+			break;
+		}
+		else
+		{
+			cout << "I am disappointed in you" << endl;
+			continue;
+		}
+
+	} while (true);
+	cout << endl;
+
+	int b = 101;
+	for (int c = 1; c < b; c++)
+	{
+		cout << c << endl;
+	}
+
+	cout << endl;
+
+	int f = 100;
+	while (f >= 1)
+	{
+		cout << f << endl;
+		f--;
+	}
+
+	cout << endl;
+
+	int g = 1995;
+	do
+	{
+		cout << g << endl;
+		g++;
+	} while (g < 2018);
+*/
+	//do
+	//{
+	//	int x;
+
+	//	cout << "What is your favorite positive number?" << endl;
+	//	cout << ">";
+	//	cin >> x;
+	//	if (x >= 0)
+	//	{
+	//		cout << "Okay, Your first number is " << x << "." << endl;
+	//		break;
+	//	}
+	//	else
+	//	{
+	//		cout << "That is not a positive number" << endl;
+	//		continue;
+
+	//	}
+	//} while (true);
+	//do{
+	//	int y;
+	//	cout << "Whats your second?" << endl;
+	//	cout << ">";
+	//	cin >> y;
+	//	if (y >= 0)
+	//	{
+	//		cout << "Your second favorite is " << y << endl;
+	//		break;
+	//	}
+	//	else
+	//	{
+	//		cout << "That isnt a positive number" << endl;
+	//	}
+	//} while (true);
+
+	//do {
+	//	int z;
+	//	cout << "What is your third favorite?" << endl;
+	//	cout << ">";
+	//	cin >> z;
+	//	if (z >= 0)
+	//	{
+	//		cout << "Your final favorite is " << z << endl;
+	//		break;
+	//	}
+	//	else
+	//	{
+	//		cout << "that is not a positive number" << endl;
+	//		continue;
+	//	}
+	//} while (true);
+
+//int min;
+//int max;
+//cin >> min;
+//cout << endl;
+//cin >> max;
+//cout << endl;
+//while (min <= max)
+//{
+//	cout << min << endl;
+//	min++;
+//}
+
+
+
+	system("pause");
+}
